8cf3a15a8ba02a3c5e39da7d1d3046be961b6e9f
Connor Lake <vertoforce@gmail.com>
2021-05-26 17:35:03 -0400

    Added accepting http client to New

--- client.go
+++ client.go
@@ -17,12 +17,16 @@ type Client struct {
 }
 
 // New creates a new client with the rate limit set up
-func New(NormalRequestToken, JavascriptRequestToken string) *Client {
+func New(NormalRequestToken, JavascriptRequestToken string, client *http.Client) *Client {
+	if client == nil {
+		client = http.DefaultClient
+	}
+
 	// ProxyCrawl has a rate limit of 20 requests per second, which means approx 1 request every 50 milliseconds
 	// We set it to conservatively 1 request every 60 milliseconds
 	rateLimit := ratelimit.NewBucketWithQuantum(time.Millisecond*60, 1, 1)
 	return &Client{
-		HttpClient:             http.DefaultClient,
+		HttpClient:             client,
 		NormalRequestToken:     NormalRequestToken,
 		JavascriptRequestToken: JavascriptRequestToken,
 		rateLimit:              rateLimit,
